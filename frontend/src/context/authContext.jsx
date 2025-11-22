d9dacbb82d53fd6d080c632571189e1719e1b650
Gopinath09062005 <gopi636936@gmail.com>
2025-11-22 12:54:34 +0530

    add emp

--- frontend/src/context/authContext.jsx
+++ frontend/src/context/authContext.jsx
@@ -15,7 +15,7 @@ const AuthContext = ({children}) => {
                 const token = localStorage.getItem('token')
                 if(token){
 
-                const response = await axios.get('http://localhost:5000/api/auth/verify',
+                const response = await axios.get('https://mern-ems-project-server.vercel.app/api/auth/verify',
                 //  const response = await axios.get(`${import.meta.env.VITE_API_URL}/auth/verify`,   ///
                     {
                   headers: {
