458d480951889ad0dfa6c51a72f0e6801f49925d
Gopinath09062005 <gopi636936@gmail.com>
2025-11-22 13:03:17 +0530

    final commits

--- frontend/src/components/employee/Add.jsx
+++ frontend/src/components/employee/Add.jsx
@@ -35,7 +35,7 @@ const Add = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/employee/add",
+        "https://mern-ems-project-server.vercel.app/api/employee/add",
         formDataObj,
         {
           headers: {
